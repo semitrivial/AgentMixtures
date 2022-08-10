6b198f6acf86bee8641f77316df80fe0d9a0b094
******* <abc@def.xyz>
2022-08-11 00:34:22 +1000

    automatic update

--- code/scan_tapped_gym.py
+++ code/scan_tapped_gym.py
@@ -8,42 +8,65 @@ import stable_baselines3 as sb3
 
 from numpy import array,float32
 
-class FakeEnv:
-    def reset(self,*a,**ka):
-        print('\nR',end='')
-        self.flip = 0
-        return array([0.02850769,0.04330681,0.04995482,0.03192485])
-    def step(self,*a,**ka):
-        self.flip = 1-self.flip
-        b = not self.flip
-        print(int(b),end='')
-        return (
-            array([ 0.09642447,  0.83463216, -0.22616503, -1.4688144 ],
-            ), 1.0, b, {})
-
-env = FakeEnv();
-env.observation_space = gym.spaces.Box(array([-4.8000002e+00,-3.4028235e+38,-4.1887903e-01,-3.4028235e+38]),array([4.8000002e+00,3.4028235e+38,4.1887903e-01,3.4028235e+38]), (4,), float32)
-
-env.action_space = gym.spaces.Discrete(2)
-env.metadata={}
-
 import os
 
 if (os.system('test -s stable_baselines3')):
     print('needs sb3 repo here to scan the source code')
     exit(0)
 
+all_envs = [env_spec.id for env_spec in gym.envs.registry.all()]
 import subprocess
 all_policies = subprocess.check_output("grep -ho '[a-zA-Z]*Policy' -r stable_baselines3 --include \*.py | sort | uniq",shell=True).decode().split('\n')
 
+step_end = '\n' 
+
+import inspect
 for k in sb3.__dict__:
-    for pol in all_policies:
-        try:
-            model = sb3.__dict__[k](pol, env)
-            print(k,pol)
-            #print(model,type(model.policy))
-            model.learn(2)
-            print("+++++++++++++++++++++++++++++++++++++++++++")
-        except:
-            #print('failed by',k,pol)
-            pass
+    if k[:2]!='__' and not inspect.ismodule(sb3.__dict__[k]):
+        for pol in all_policies:
+            for env0 in all_envs:
+                try:
+                    print('\nSTART',k,pol,env0)
+                    env = gym.make(env0)
+                    e = env
+                    for i in range(99999):
+                       f = e.step
+                       def f1(*args, **kwargs):
+                          #print('   ===================')
+                          #print('   ',i,f,args,kwargs)
+                          #for line in traceback.format_stack():
+                          #   print('   ',line.strip())
+                          #print('   ===================')
+                          r = f(*args,**kwargs)
+                          print(int(r[-2]),end=step_end)
+                          return r
+                       e.step = f1
+                       del f1
+                       #del f
+                       fR = e.reset
+                       def f2(*args, **kwargs):
+                          #print('+++++++++++++++++++++')
+                          #print(i,fR,args,kwargs)
+                          #for line in traceback.format_stack():
+                          #   print(line.strip())
+                          #print('+++++++++++++++++++++')
+                          r = fR(*args,**kwargs)
+                          #print('RRRR', r)
+                          print('R',end=step_end)
+                          return r
+                       e.reset = f2
+                       del f2
+                       #del f
+                       if 'env' in e.__dict__:
+                          e = e.env
+                       else:
+                          break
+
+                    model = sb3.__dict__[k](pol, env)
+                    print(k,pol)
+                    #print(model,type(model.policy))
+                    model.learn(2)
+                    print("#############################################")
+                except Exception as e:
+                    print('FAIL',k,pol,e)
+                    pass
