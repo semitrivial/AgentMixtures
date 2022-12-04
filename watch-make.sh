12c60bddfe3566b8c2ec07d065e911435ba230c4
******* <abc@def.xyz>
2022-12-05 00:47:24 +1000

    adding safety Implication

--- watch-make.sh
+++ watch-make.sh
@@ -1,1 +1,2 @@
-watchexec -w main.tex -r -s SIGKILL -- bash -c \"pdflatex\ main.tex\;bibtex\ main\"
+#watchexec -w main.tex -r -s SIGKILL -- bash -c \"pdflatex\ main.tex\;bibtex\ main\"
+watchexec -w author_response.tex -r -s SIGKILL -- bash -c \"pdflatex\ author_response.tex\;bibtex\ main\"
